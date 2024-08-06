4e99e11f33904000284b5efb263ba10e8014e3f6
shumei.zwq <shumei.zwq@raycloud.com>
2024-08-06 13:54:26 +0800

    test

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -70,14 +70,14 @@ func Test_cni0(t *testing.T) {
 }
 
 func Test_eth0(t *testing.T) {
-	config := cni0.NewPacketClientConfig()
+	config := eth0.NewPacketClientConfig()
 	config.ProcessKeyword = []string{"java", "curl"}
 	// config.ServiceAddr = map[string]string{
 	// 	"192.168.165.xx:30002":  "zk-1",
 	// 	"192.168.162.1xx:30002": "zk-2",
 	// 	"192.168.162.1xx:30003": "zk-3",
 	// }
-	client := cni0.NewPacketClient(config)
+	client := eth0.NewPacketClient(config)
 	result := client.Run()
 	fmt.Print(result)
 }
