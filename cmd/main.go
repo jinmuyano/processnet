424197e96549077d6dfdbbcc17fb9d6c97ec33e1
shumei.zwq <shumei.zwq@raycloud.com>
2024-08-15 14:06:25 +0800

    updateupdate

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"sort"
 	"strconv"
 
@@ -81,32 +80,4 @@ func main() {
 	// 打印进程出流量
 	fmt.Println("进程出流量:", pidnet.OutRate/1024/1024, "mb/s")
 
-	// 计算哪个服务调用
-	// 服务入流量
-	var in_svc map[string]int64
-	for ipport, v := range pidnet.InService {
-		// 执行cmd命令查询ipport属于哪个服务
-		cmd := fmt.Sprintf("sh /data/git2.superboss.cc/shell/shell.sh %s", ipport)
-		cmd_out, _ := exec.Command("/bin/sh", "-c", cmd).Output()
-		in_svc[string(cmd_out)] += v
-	}
-
-	// 服务出流量
-	var out_svc map[string]int64
-	for ipport, v := range pidnet.InService {
-		// 执行cmd命令查询ipport属于哪个服务
-		cmd := fmt.Sprintf("sh /data/git2.superboss.cc/shell/shell.sh %s", ipport)
-		cmd_out, _ := exec.Command("/bin/sh", "-c", cmd).Output()
-		out_svc[string(cmd_out)] += v
-	}
-
-	// 打印服务入流量
-	fmt.Println("服务入流量")
-	for k, v := range in_svc {
-		fmt.Printf("%s-> %dmb/s\n", k, v/1024/1024)
-	}
-	fmt.Println("服务出流量")
-	for k, v := range out_svc {
-		fmt.Printf("%s-> %dmb/s\n", k, v/1024/1024)
-	}
 }
